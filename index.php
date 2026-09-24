74bb0a1c0a12159d0f714020e9f22a8f4600d45a
nescryo <muhammadryo15@gmail.com>
2026-09-24 10:21:23 +0800

    add package

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Classes\Plugin;
+require 'vendor/autoload.php';
 
-return new class extends Plugin
-{
-    public function boot()
-    {
-    }
-};
+use StripePayment\StripePaymentPlugin;
+
+return new StripePaymentPlugin;
---
+++ src/StripePaymentPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace StripePayment;
+
+use App\Classes\Plugin;
+
+class StripePaymentPlugin extends Plugin
+{
+    public function boot()
+    {
+        
+    }
+}
